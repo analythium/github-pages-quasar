fb146146ad459e90b43bb10458455c3317cd27d1
Peter Solymos <psolymos@gmail.com>
2020-04-03 23:45:53 -0600

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,18 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page class="flex">
+    <h4>Github action for deploying Quasar app to GitHub pages</h4>
+    <dev class="text-body1">
+      See the
+      <a href="https://github.com/analythium/github-pages-quasar"
+        >analythium/github-pages-quasar</a
+      >
+      GitHub repository.
+    </dev>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+};
 </script>
